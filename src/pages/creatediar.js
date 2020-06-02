9746ca4e5c49afa21a32e7e17a405bbd4eaff3ce
ydalkilic <ysndlklc1234@gmail.com>
2020-06-02 21:16:40 +0300

    working diary multiple files

--- src/pages/creatediar.js
+++ src/pages/creatediar.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useRef, useState} from "react";
 import Card from "../components/card";
 import MyEditor from "../components/editor";
 import InputForm from "../components/formInput";
@@ -11,16 +11,94 @@ import {msgBox} from "../utils/appmsgbox";
 import DiaryContext from "../context/diaryContext";
 import Loading from "../components/loading";
 import PageSubHeader from "../components/pagesubheader";
+import {ImageValidator} from "../utils/appimagevalidator";
+import {readFileBase64} from "../utils/appimageconverter";
+import axios from "axios";
+import header from "../utils/axiosheader";
 
 const CreateDiary = () => {
+    let creatediarfileInput = useRef()
     const [diartext, setDiarText] = useState("")
     const [diartitle, setDiarTitle] = useState("")
     const {user} = useContext(GlobalContext)
     const {creatediary, state, dispatch} = useContext(DiaryContext)
+    const [diarimages, setdiarimages] = useState([])
+    const [uploadedImages, setUploadedImages] = useState([])
     const onChangeEditor = (e) => {
         setDiarText(e)
     }
+    const changeImage = () => {
+        debugger
+        let result = true
+        let base64Images = []
+        let uploadImg = [];
+        creatediarfileInput.current.files.forEach((file) => {
+            debugger
+            if (ImageValidator(file) === false) {
+            } else {
+                debugger
+                uploadImg.push(file)
+                setUploadedImages([])
+                setUploadedImages(uploadImg)
+                readFileBase64(file)
+                    .then((res) => {
+                        res.decode64 = res.base64
+                        res.base64 = 'data:image/png;base64,' + res.base64
+                        base64Images.push(res)
+                        setdiarimages([])
+                        setdiarimages(base64Images)
+                    }).catch((error) => {
 
+                })
+            }
+        })
+        console.log("rs", uploadImg)
+    }
+    const topludosya = () => {
+        debugger
+        console.log("t", uploadedImages)
+        let formData = new FormData();
+        console.log(diarimages)
+        for (let i = 0; i < uploadedImages.length; i++) {
+            var file = uploadedImages[i]
+            formData.append('files', file);
+        }
+        debugger
+        axios.post("http://127.0.0.1:3000/test", formData, header('multipart/form-data')).then((response) => {
+            debugger
+        }).catch((error) => {
+            debugger
+        })
+    }
+    const removePreviewImage = (index) => {
+        const images = JSON.parse(JSON.stringify(diarimages));
+        images.splice(index, 1)
+        setdiarimages([])
+        setdiarimages(images)
+    }
+    const renderPreview = () => {
+        if (diarimages.length > 1) {
+            return diarimages.map((image, index) => {
+                return (
+                    <React.Fragment key={index + "fragment"}>
+                        <img
+                            key={index + "img"}
+                            src={image.base64}
+                            alt="image"
+                            className="img-fluid avatar-xl pl-2 pt-2"/>
+                        <Button buttonclases={"btn-danger btn-xs btn-sm"}
+                                onClick={() => {
+                                    removePreviewImage(index)
+                                }}
+                                key={index + "btn"}
+                                type={"button"} icon={"fa fa-times"}/>
+
+                    </React.Fragment>
+
+                )
+            })
+        }
+    }
     const resetForm = () => {
         //dosyalara bakılacak
         setDiarTitle("")
@@ -93,7 +171,43 @@ const CreateDiary = () => {
                     <div className="row">
                         <div className="col-lg-6">
                             <Card>
-                                Resim Yükleme
+                                <p className="text text-center"> Resim Yükleme
+                                </p>
+                                <div className="form-group mb-3 mt-3">
+                                    <a
+                                        onClick={() => {
+                                            creatediarfileInput.current.click();
+                                        }}
+                                        aria-pressed="true"
+                                        className="btn btn-block btn-rounded btn-success text-white"
+                                        role="button"
+                                    >Resim Seç</a>
+                                    <input className={"form-control"} multiple hidden type={"file"}
+                                           ref={creatediarfileInput}
+                                           name="myImage"
+                                           onChange={() => {
+                                               changeImage()
+                                           }}
+                                           accept="image/*"
+                                           style={{display: "none"}}
+                                    />
+
+                                </div>
+                                <div className="form-group">
+                                    <p className="text text-center font-20 font-weight-bold mt-3 pb-2">
+                                        {diarimages.length > 1 ? 'Önizleme' : ''}
+
+                                    </p>
+
+                                    {renderPreview()}
+                                    <p className="text text-center mt-3 pb-2">{
+                                        diarimages.length > 1 ? 'Toplam Resim ' + diarimages.length : ''
+                                    }</p>
+                                    <button onClick={() => {
+                                        topludosya()
+                                    }}> Tıkla
+                                    </button>
+                                </div>
                             </Card>
 
                         </div>
